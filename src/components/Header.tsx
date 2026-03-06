64a6e8a01a90b478e0baf9165ca57e189a7c02b4
stackovermaw <johncarlo.camara1@gmail.com>
2026-03-06 11:23:43 +0800

    feat: add initial header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,60 @@
 import { Link } from "@tanstack/react-router";
+import { RefreshCw, Wifi, WifiOff } from "lucide-react";
+import { useState } from "react";
+import { sileo } from "sileo";
+
 import ThemeToggle from "./ThemeToggle";
+import { Button } from "./ui/button";
+import { TypographyH3 } from "./ui/h3";
 
 export default function Header() {
-  return <header className="sticky top-0 z-50"></header>;
+  const now = new Date();
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  return (
+    <header className="sticky top-0 z-50  p-5 border-b border-(--border-base) flex justify-between">
+      <section>
+        <TypographyH3>Lotto Results</TypographyH3>
+        <span>
+          {new Intl.DateTimeFormat("en-PH", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            timeZone: "Asia/Manila",
+          }).format(now)}
+        </span>
+      </section>
+
+      <section className="flex items-center gap-2">
+        {navigator.onLine ? (
+          <Wifi className="text-green-500" size={18} />
+        ) : (
+          <WifiOff size={18} />
+        )}
+
+        <Button
+          size="icon"
+          variant="outline"
+          disabled={isRefreshing}
+          onClick={() => {
+            setIsRefreshing((prev) => !prev);
+
+            setTimeout(() => {
+              setIsRefreshing(false);
+              sileo.success({
+                title: "Refreshed",
+                description: "The lotto results have been refreshed.",
+              });
+            }, 2000);
+          }}
+        >
+          <RefreshCw
+            className={`transition-transform ${isRefreshing ? "animate-spin" : ""}`}
+            size={18}
+          />
+        </Button>
+        <ThemeToggle />
+      </section>
+    </header>
+  );
 }
