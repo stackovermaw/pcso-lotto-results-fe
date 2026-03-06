8a412215bc8d916789cd22d0cb3c12d2346448a2
stackovermaw <johncarlo.camara1@gmail.com>
2026-03-07 00:55:15 +0800

    feat: add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,21 @@
+export const MONTHS = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
+];
+
+export const LOCALE = "en-PH";
+
+export const LOCALE_OPTIONS = {
+  hour12: false,
+  timeZone: "Asia/Manila",
+};
